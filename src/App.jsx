f5b7a71c66474fef7f331284924a0c3e7adf1014
ArpitSingh73 <arpitsingh73073@gmail.com>
2023-11-30 17:32:19 +0530

    alert added

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import "./App.css";
 
 const App = () => (
   <div className="App">
+    {alert("Game is not developed keeping small devices in mind. It will for sure ill-perform.")}
     <Board></Board>
   </div>
 );
