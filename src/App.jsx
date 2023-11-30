80b81fd57e661c439ac5dcd26cd2748a144ba200
ArpitSingh73 <arpitsingh73073@gmail.com>
2023-11-30 18:29:18 +0530

    alert modified.

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import "./App.css";
 
 const App = () => (
   <div className="App">
-    {alert("Better to check on PC & Desktops as game is not developed keeping small devices in mind. It will for sure ill-perform!")}
+    {alert("Better to check on PC & Desktops as game is not developed keeping small devices in mind. It will for sure ill-perform! Still in desktop mode game can we completely viewed.")}
     <Board></Board>
   </div>
 );
